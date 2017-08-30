a571a371cd74d3a0943dbb730adca596a667d8e2
Adam Rosenblum <adamrosenblum@Adams-MBP.attlocal.net>
2017-08-29 20:16:50 -0700

    started adding reducers

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,50 +1,39 @@
 // Generate a random number
-export const RANDOM_NUMBER = 'RANDOM_NUMBER';
-export const randomNumber = random => ({
-    type: RANDOM_NUMBER,
-    random
+export const START_GAME = 'START_GAME';
+export const startGame = (randomNumber) => ({
+    type: START_GAME,
+    startGame: randomNumber
 });
 
 // Guess a number
 export const GUESS_NUMBER = 'GUESS_NUMBER';
-export const guessNumber = guess => ({
+export const guessNumber = (guess) => ({
     type: GUESS_NUMBER,
-    guess
+    guess: guess
 });
-
-const action = guessNumber('You Guessed [X]');
 
 // If the number is further from the random number
 export const COLDER = 'COLDER';
-export const further = lowGuess => ({
-    type: CODLER,
-    lowGuess
+export const further = (guessedNumber) => ({
+    type: COLDER,
+    guess: guessedNumber
 });
-
-const action = tooLow('You Are Getting Colder');
-
 
 // If the number is closer from the random number
 export const HOTTER = 'HOTTER';
-export const closer = closerGuess => ({
+export const closer = (guessedNumber) => ({
     type: HOTTER,
-    closerGuess
+    guess: guessedNumber
 });
-
-const action = closer('You Are Getting Hotter');
 
 // If the number is exact - tell them they won 
 export const TARGET = 'TARGET';
-export const onTarget = exact => ({
-    type: 'TARGET',
-    exact
+export const onTarget = () => ({
+    type: 'TARGET'
 });
-
-const action = exact("Congrats...you guessed the random number....here's a cookie");
 
 // Let them play again
 export const PLAY_AGAIN = 'PLAY_AGAIN';
-export const restart = playAgain => ({
-    type: PLAY_AGAIN,
-    playAgain
+export const restart = () => ({
+    type: PLAY_AGAIN
 });
