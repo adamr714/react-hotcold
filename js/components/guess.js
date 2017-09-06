c3f664160e35276bf4d87c816c3c61b77369585a
Adam Rosenblum <adamR714@gmail.com>
2017-09-05 20:24:11 -0700

    updates

--- js/components/guess.js
+++ js/components/guess.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
+import * as actions from '../actions/index';
+import store from '../store';
+
+
 export default class Guess extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
         };
-
         this.onFormSubmit = this.onFormSubmit.bind(this);
     }   
     
-    onFormSubmit(event) {
-      event.preventDefault();
-    }
+    onFormSubmit(e) {
+        e.preventDefault();
+        let numberGuessed = this.refs.myInput.value;
+        console.log(numberGuessed);
+        store.dispatch(actions.guessNumber(numberGuessed));
+    }    
 
     render() {
         return (
             <div className="guess">
-                <form ref="guessInput">
-                    <input type="text" ref="myInput" onChange={this.onAddInputChange} />
+                <form ref="guessInput" onSubmit={this.onFormSubmit}>
+                    <input type="text" ref="myInput" />
                     <input type="submit" value="submit" />
                 </form>
             </div>    
--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,20 +1,20 @@
 import * as actions from '../actions/index';
-import store from './store';
+import store from '../store';
 
-const initialRepoistoryState = Math.floor((Math.random() * 100) + 1);
+// const initialState = Math.floor((Math.random() * 100) + 1);
+const initialState=[];
 
-const reducer = (state, action) => {
-    state = state || initialState;
-    return {
-        guess: boardReducer(state.guess, action),
-    };
-}
+// const reducer = (state, action) => {
+//     state = state || initialState;
+//     return {
+//         guess: boardReducer(state.guess, action),
+//     };
+// }
 
-export const repositoryReducer = (state=initialRepositoryState, action) => {
+
+export const guessReducer = (state=initialState, action) => {
     if (action.type === actions.GUESS_NUMBER) {
-        return [...state, {
-            name: action.guess
-        }];        
+   
     }
 
     else if (action.type === action.TARGET) {
